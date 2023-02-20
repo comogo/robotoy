9f29a52156442099a91101071b0ab2c654aa9d0c
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2023-02-20 09:31:11 -0300

    temp

--- rust/src/radio.rs
+++ rust/src/radio.rs
@@ -15,12 +15,17 @@ const CMD_W_REGISTER: Command = 0x20;
 
 const REG_CONFIG: Register = 0x00;
 const REG_EN_AA: Register = 0x01;
+const REG_EN_RXADDR: Register = 0x02;
 const REG_CH: Register = 0x05;
 const REG_RF: Register = 0x06;
 const REG_RX_ADDR_P0: Register = 0x0A;
 const REG_TX_ADDR: Register = 0x10;
 const REG_DYNPD: Register = 0x1C;
 const REG_FEATURE: Register = 0x1D;
+
+const BIT_FEATURE_EN_DYN_ACK: u8 = 0b0000_0001;
+const BIT_FEATURE_EN_ACK_PAY: u8 = 0b0000_0010;
+const BIT_FEATURE_EN_DPL: u8 = 0b0000_0100;
 
 #[derive(Debug)]
 pub enum DeviceError {
@@ -88,6 +93,7 @@ impl Device {
         Ok((response[0], response[1]))
     }
 
+    /// Set the data rate and the power level.
     pub fn set_rf(&self, data_rate: DataRate, power_level: Power) -> Result<(), DeviceError> {
         let rate: u8 = match data_rate {
             DataRate::_250Kbps => 0b0010_0000,
@@ -106,6 +112,16 @@ impl Device {
         Ok(())
     }
 
+    /// Function used to enable/disable the dynamic payload length.
+    pub fn set_dynamic_payload(&self, enable: bool) -> Result<(), DeviceError> {
+        let mut response = [0u8; 2];
+        let value: u8 = if enable { 0b0011_1111 } else { 0b0000_0000 };
+        self.command(&mut [CMD_W_REGISTER | REG_DYNPD, value], &mut response)?;
+        Ok(())
+    }
+
+    /// Enable only the pipe 0 and set its address.
+    /// The address is 5 bytes long.
     pub fn set_rx_address(&self, address: [u8; 5]) -> Result<(), DeviceError> {
         let mut response = [0u8; 6];
         self.command(
@@ -119,16 +135,12 @@ impl Device {
             ],
             &mut response,
         )?;
+        self.write_register(CMD_W_REGISTER | REG_EN_RXADDR, 0x01)?;
         Ok(())
     }
 
-    pub fn set_dynamic_payload(&self, enable: bool) -> Result<(), DeviceError> {
-        let mut response = [0u8; 2];
-        let value: u8 = if enable { 0b0011_1111 } else { 0b0000_0000 };
-        self.command(&mut [CMD_W_REGISTER | REG_DYNPD, value], &mut response)?;
-        Ok(())
-    }
-
+    /// Set the address used for the transmission.
+    /// The address is 5 bytes long.
     pub fn set_tx_address(&self, address: [u8; 5]) -> Result<(), DeviceError> {
         let mut response = [0u8; 6];
         self.command(
@@ -145,6 +157,7 @@ impl Device {
         Ok(())
     }
 
+    /// Enable or disable the auto acknowledgment for all pipes.
     pub fn set_auto_ack(&self, enable: bool) -> Result<(), DeviceError> {
         let mut response = [0u8; 2];
         let value: u8 = if enable { 0b0011_1111 } else { 0b0000_0000 };
@@ -152,6 +165,7 @@ impl Device {
         Ok(())
     }
 
+    /// Set the channel used for the communication.
     pub fn set_channel(&self, channel: u8) -> Result<(), DeviceError> {
         if channel > 125 {
             return Err(DeviceError::InvalidChannel);
@@ -161,6 +175,11 @@ impl Device {
         Ok(())
     }
 
+    /// Configure the NRF24L01+ features.
+    /// The features are:
+    /// - Dynamic payload length
+    /// - Payload with ACK
+    /// - Dynamic ACK
     pub fn set_feature(&self, value: u8) -> Result<(), DeviceError> {
         self.write_register(REG_FEATURE, value)?;
         Ok(())
@@ -198,25 +217,24 @@ impl Radio {
         })
     }
 
+    // Configure the NRF24L01+ to use 250Kbps data rate, 0dBm power, dynamic payload, and disable auto ack
     pub fn setup(&self) -> Result<(), DeviceError> {
+        self.device
+            .set_feature(BIT_FEATURE_EN_DPL | BIT_FEATURE_EN_DYN_ACK)?;
         self.device.set_rf(DataRate::_250Kbps, Power::_0dBm)?;
         self.device.set_dynamic_payload(true)?;
         self.device.set_auto_ack(false)?;
-
-        // Enable dynamic payload length and NOACK
-        // EN_DPL = 1
-        // EN_ACK_PAY = 0
-        // EN_DYN_ACK = 1
-        self.device.set_feature(0b0000_0101)?;
         Ok(())
     }
 
-    pub fn set_channel(&self, channel: u8) {
-        self.device.set_channel(channel);
+    pub fn set_channel(&self, channel: u8) -> Result<(), DeviceError> {
+        self.device.set_channel(channel)?;
+        Ok(())
     }
 
-    pub fn set_address(&self, tx_address: [u8; 5], rx_address: [u8; 5]) {
-        self.device.set_rx_address(rx_address);
-        self.device.set_tx_address(tx_address);
+    pub fn set_address(&self, tx_address: [u8; 5], rx_address: [u8; 5]) -> Result<(), DeviceError> {
+        self.device.set_rx_address(rx_address)?;
+        self.device.set_tx_address(tx_address)?;
+        Ok(())
     }
 }
