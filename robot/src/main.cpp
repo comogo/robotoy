2f155fcbc605a47362acaea18cff427e5396c826
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2023-03-19 09:45:00 -0300

    Use the pin 9 to control the servo motor
    - LED turns on when initializing and turns off when
      radio is connected.
    - The left stick control the front wheel direction.
      Turn left/right.

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -1,28 +1,42 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "RF24.h"
+#include <Servo.h>
 #include <motor.h>
 #include <controller.h>
 
+/*
+  PWM pins: 3, 5, 6, 9, 10, 11
+*/
+
 #define MOTOR_STANDBY 2
-#define MOTOR_PWM 3
+#define MOTOR_PWM 3 // PWM
 #define MOTOR_FORWARD 4
 #define MOTOR_BACKWARD 5
+#define LED 6 // PWM
 #define RADIO_CE 7
 #define RADIO_CSN 8
-#define LED 6
+#define SERVO 9 // PWM
+
 
 RF24 radio(RADIO_CE, RADIO_CSN);
 Motor motor(MOTOR_STANDBY, MOTOR_PWM, MOTOR_FORWARD, MOTOR_BACKWARD);
+Servo servo;
 Controller controller;
 uint8_t address[][6] = {"aaaaa", "aaaaa"};
 uint8_t payload[13];
 uint8_t speed = 0;
 uint8_t lastSpeed = speed;
-uint8_t direction = 0; // 0 = BREAK; 1 = FORWARD; 2 = BACKWARD
+uint8_t direction = 0;
+int16_t rotation = 90;
+int16_t lastRotation = 0;
+bool radioConnected = false;
 
 void setup()
 {
+  pinMode(LED, OUTPUT);
+  analogWrite(LED, 100);
+
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
@@ -33,9 +47,9 @@ void setup()
   // radio.openReadingPipe(1, address[1]);
   radio.startListening();
 
-  pinMode(LED, OUTPUT);
-  analogWrite(LED, speed);
   motor.initialize();
+  servo.attach(SERVO);
+  servo.write(rotation);
 }
 
 void loop() {
@@ -45,7 +59,19 @@ void loop() {
     uint8_t bytes = radio.getPayloadSize();
     radio.read(&payload, bytes);
     controller.load_state_from_payload(payload);
+
+    if (!radioConnected) {
+      radioConnected = true;
+      analogWrite(LED, 0);
+    }
+
     speed = 0;
+
+    rotation = map(controller.getYaw(), -32768, 32767, 180, 0);
+
+    if (rotation != lastRotation) {
+      servo.write(rotation);
+    }
 
     if (controller.getL2() == 0 && controller.getR2() == 0) {
       motor.stop();
@@ -70,4 +96,5 @@ void loop() {
   }
 
   lastSpeed = speed;
+  lastRotation = rotation;
 }
